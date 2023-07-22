41b151420fa92376efa4f8519fc91ddcdd0dd848
prajwal <prajwalshah912@gmail.com>
2023-07-22 16:38:50 +0530

    encapsulate the styles

--- src/app/server-component/server-component.component.ts
+++ src/app/server-component/server-component.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-component',
   templateUrl: './server-component.component.html',
-  styleUrls: ['./server-component.component.css']
+  styleUrls: ['./server-component.component.css'],
+  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None is used to remove the view encapsulation of the dom to remove
+  // the wierd attribute in the selector css -- it has no shadow dom the style can apply to any elem if no class is given
 })
 export class ServerComponentComponent implements OnInit {
   @Input('serverComp') element:{type:string, name:string, content:string};
